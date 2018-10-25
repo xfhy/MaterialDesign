e452e60ef9cab0ceac6705d06da5e2e98901483c
xfhy666 <xfhy666@gmail.com>
2018-10-25 15:23:08 +0800

    MaterialButton

---
+++ app/src/main/java/com/xfhy/material/MaterialButtonActivity.java
@@ -0,0 +1,18 @@
+package com.xfhy.material;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 2018年10月25日15:13:13
+ * Material Button
+ */
+public class MaterialButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_button);
+    }
+}
